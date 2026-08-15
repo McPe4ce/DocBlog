04b6c7ebb6c376035f50ebefb7eb72b266f5b207
Mcpe4ce <phil.ghanem@gmail.com>
2026-08-15 12:23:26 +0200

    Update index view and template to display dynamic user name and current date

--- src/Docblog/views.py
+++ src/Docblog/views.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "index.html", context={"name": "Philip"})
+    date = datetime.today()
+    return render(request, "index.html", context={"name": "Philip", "date": date})
