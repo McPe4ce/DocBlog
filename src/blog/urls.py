1b05df10a296a5eddd2f6a6784a70ddb3da1928a
Mcpe4ce <phil.ghanem@gmail.com>
2026-08-16 11:42:44 +0200

    Add article view and templates for blog articles with error handling

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from blog.views import index
+from blog.views import index, article
 
 urlpatterns = [
     path('', index, name="blog-index"),
+    path('article-<str:num_article>/', article, name="blog-article"),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request, "blog/index.html")
+
+def article(request, num_article):
+    if num_article not in ["01", "02", "03"]:
+        return render(request, f"blog/article_not_found.html")
+    return render(request, f"blog/article_{num_article}.html")
