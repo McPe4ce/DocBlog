b5c1f03af844046a1356d70ef738376121a34924
Mcpe4ce <phil.ghanem@gmail.com>
2026-08-16 12:10:32 +0200

    Add static files configuration and update templates to include CSS

--- src/Docblog/settings.py
+++ src/Docblog/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "Docblog/static"),
+]
 
 
 # Email
