7df588c92d2855d9a39e667bfe7790bb5e6a2969
Mcpe4ce <phil.ghanem@gmail.com>
2026-08-16 11:21:20 +0200

    Update templates and views for DocBlog and Blog pages
    - Add index.html template for DocBlog with dynamic content
    - Create index.html template for Blog page
    - Update views to render the correct templates
    - Add __pycache__ to .gitignore
    - Remove obsolete index.html from Docblog templates

--- src/Docblog/views.py
+++ src/Docblog/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 def index(request):
     date = datetime.today()
-    return render(request, "index.html", context={"name": "Philip", "date": date})
+    return render(request, "Docblog/index.html", context={"name": "Philip", "date": date})
